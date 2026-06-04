252ae9e4a45847ba844e2339165b305448830b33
Haruya <cunshangyaozai@gmail.com>
2026-06-05 00:11:32 +0900

    6/5 BOSSが地形を壊せるようにしました

--- Assets/Scripts/Boss/Boss.cs
+++ Assets/Scripts/Boss/Boss.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
+    [SerializeField]
+    private float destructRadius = 0.5f;
+    [SerializeField]
+    private CrackParameter crackParameter;
+
     Rigidbody2D rb;
     public float speed = 3.0f;
     public float moveHeight = 2.0f;
     public float moveSpeed = 2.0f;
 
     float startY;
+    private float destructTimer;
 
     private void Start()
     {
@@ -21,11 +27,30 @@ public class Boss : MonoBehaviour
         rb.MovePosition(new Vector2(transform.position.x + speed * Time.fixedDeltaTime, y));
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
+        Debug.Log("触れている: " + collision.name);
+
         if (collision.CompareTag("Field"))
         {
-            Destroy(collision.gameObject);
+            Debug.Log("Fieldタグに当たった");
+
+            TerrainContext terrain = collision.GetComponent<TerrainContext>();
+
+            if (terrain == null)
+            {
+                Debug.Log("TerrainContextが見つからない");
+                return;
+            }
+
+            destructTimer += Time.deltaTime;
+
+            if (destructTimer >= 0.2f)
+            {
+                Debug.Log("地形破壊実行");
+                terrain.Destruct(transform.position, destructRadius, crackParameter);
+                destructTimer = 0.0f;
+            }
         }
     }
 }
