fda78e366faf97ff7a4754f715f5d0ac33c87ae3
rei1205 <rei1205.creator@gmail.com>
2026-06-14 15:17:28 +0900

    ゲームシーン構築

--- Assets/Temp/LightRotation.cs
+++ Assets/Temp/LightRotation.cs
@@ -5,13 +5,8 @@ public class LightRotation : MonoBehaviour
     [SerializeField]
     private float _rotateSpeed;
 
-    [SerializeField]
-    private bool _isActive;
-
-    // Update is called once per frame
     void Update()
     {
-        if (_isActive)
-            transform.Rotate(0.0f, 0.0f, _rotateSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(0.0f, 0.0f, _rotateSpeed * Time.deltaTime, Space.World);
     }
 }
