08b52fe279feca49522a8a0651239aa5ba48cdbb
Haruya <cunshangyaozai@gmail.com>
2026-06-30 17:06:19 +0900

    6/30 コードを見やすくしました

--- Assets/Scripts/Boss/Boss.cs
+++ Assets/Scripts/Boss/Boss.cs
@@ -5,7 +5,6 @@ public class Boss : MonoBehaviour
     // 地形を破壊する範囲
     [SerializeField]
     private float destructRadius = 0.5f;
-
     // 地形破壊時のひび割れ設定
     [SerializeField]
     private CrackParameter crackParameter;
@@ -13,47 +12,37 @@ public class Boss : MonoBehaviour
     // プレイヤーの位置を取得するための参照
     [SerializeField]
     private Transform player;
-
     // この距離以上プレイヤーと離れたら突進する
     [SerializeField]
     private float maxDistance = 20.0f;
-
     // この距離以上プレイヤーと離れたらスピードが速くなる
     [SerializeField]
     private float middleDistance = 12.0f;
-
     // 突進後、プレイヤーからどれくらい離れた位置で止まるか
     [SerializeField]
     private float closeDistance = 10.0f;
-
     // 突進するときの速度
     [SerializeField]
     private float dashSpeed = 30.0f;
-
     // 早い時の速度
     [SerializeField]
     private float middleSpeed = 8.0f;
-
+    // 通常時の右移動速度
+    public float speed = 3.0f;
+    // 上下移動の幅
+    public float moveHeight = 2.0f;
+    // 上下移動の速さ
+    public float moveSpeed = 2.0f;
+    // 地形破壊の間隔を管理するタイマー
+    private float destructTimer;
     // 突進中かどうか
     private bool isDashing;
-
     // BossのX座標管理用
     private float moveX;
 
     // Rigidbody2D
     private Rigidbody2D rb;
 
-    // 通常時の右移動速度
-    public float speed = 3.0f;
-
-    // 上下移動の幅
-    public float moveHeight = 2.0f;
-
-    // 上下移動の速さ
-    public float moveSpeed = 2.0f;
-
-    // 地形破壊の間隔を管理するタイマー
-    private float destructTimer;
 
     private void Start()
     {
@@ -72,54 +61,15 @@ public class Boss : MonoBehaviour
         }
 
         // プレイヤーとの横方向の距離
-        float distance = Mathf.Abs(moveX - player.position.x);
+        float distanceX = Mathf.Abs(moveX - player.position.x);
 
-        // 一定以上離れたら突進開始
-        if (distance >= maxDistance)
-        {
-            isDashing = true;
-        }
-
-        if (isDashing)
-        {
-            // プレイヤーの少し後ろを目標位置にする
-            float targetX = player.position.x - closeDistance;
-
-            // 目標位置まで高速で近づく
-            moveX = Mathf.MoveTowards(
-                moveX,
-                targetX,
-                dashSpeed * Time.fixedDeltaTime
-            );
-
-            // 目標位置に近づいたら突進終了
-            if (Mathf.Abs(moveX - targetX) <= 0.1f)
-            {
-                isDashing = false;
-            }
-        }
-        else
-        {
-            if (distance >= middleDistance)
-            {
-                moveX += middleSpeed * Time.fixedDeltaTime;
-            }
-            else
-            {
-                // 通常時は右に移動する
-                moveX += speed * Time.fixedDeltaTime;
-            }
-        }
-
-        // プレイヤーのY座標を中心に上下移動する
-        float y = player.position.y + Mathf.Sin(Time.time * moveSpeed) * moveHeight;
-
-        // 計算した位置へ移動
-        rb.MovePosition(new Vector2(moveX, y));
+        BOSSSpeedState(distanceX);
+        BOSSMoveY();
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if (!collision.CompareTag("Field")) return;
         // Fieldタグの地形に触れている間だけ処理する
         if (collision.CompareTag("Field"))
         {
@@ -131,15 +81,86 @@ public class Boss : MonoBehaviour
                 return;
             }
 
-            // 一定時間ごとに破壊する
-            destructTimer += Time.deltaTime;
+            BreakTerrain(terrain);
+        }
+    }
 
-            if (destructTimer >= 0.2f)
+    //  BOSSの動きの変化
+    private void BOSSSpeedState(float distanceX)
+    {
+        // 一定以上離れたら突進開始
+        if (distanceX >= maxDistance)
+        {
+            isDashing = true;
+        }
+
+        if (isDashing)
+        {
+            LongBOSSSpeed();
+        }
+        else
+        {
+            if (distanceX >= middleDistance)
             {
-                terrain.Destruct(transform.position, destructRadius, crackParameter);
-
-                destructTimer = 0.0f;
+                MiddleBOSSSpeed();
             }
+            else
+            {
+                ShortBOSSSpeed();
+            }
+        }
+
+    }
+    // BOSSの移動処理
+    private void BOSSMoveY()
+    {
+        // プレイヤーのY座標を中心に上下移動する
+        float y = player.position.y + Mathf.Sin(Time.time * moveSpeed) * moveHeight;
+
+        // 計算した位置へ移動
+        rb.MovePosition(new Vector2(moveX, y));
+    }
+    // BOSSとPlayerが遠い時
+    private void LongBOSSSpeed()
+    {
+        // プレイヤーの少し後ろを目標位置にする
+        float targetX = player.position.x - closeDistance;
+
+        // 目標位置まで高速で近づく
+        moveX = Mathf.MoveTowards(
+            moveX,
+            targetX,
+            dashSpeed * Time.fixedDeltaTime
+        );
+
+        // 目標位置に近づいたら突進終了
+        if (Mathf.Abs(moveX - targetX) <= 0.1f)
+        {
+            isDashing = false;
+        }
+    }
+    // BOSSとPlayerが中間の時
+    private void MiddleBOSSSpeed()
+    {
+        moveX += middleSpeed * Time.fixedDeltaTime;
+    }
+    // BOSSとPlayerが近い時
+    private void ShortBOSSSpeed()
+    {
+        moveX += speed * Time.fixedDeltaTime;
+    }
+
+    // 破壊設定
+    private void BreakTerrain(TerrainContext terrain)
+    {
+        // 一定時間ごとに破壊する
+        destructTimer += Time.deltaTime;
+
+        if (destructTimer >= 0.2f)
+        {
+            terrain.Destruct(transform.position, destructRadius, crackParameter);
+
+            destructTimer = 0.0f;
         }
     }
 }
